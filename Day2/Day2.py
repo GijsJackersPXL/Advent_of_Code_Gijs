5472333acfa1c589d2660d16e1a8ef38294de867
GijsJackersPXL <gijs.jackers@student.pxl.be>
2023-05-01 16:29:00 +0100

    PART1

---
+++ Day2/Day2.py
@@ -0,0 +1,35 @@
+with open('Day2/input2.txt') as input_line:
+    score = 0
+    for line in input_line:
+        char1 = line[0]
+        char2 = line[2]
+        
+        #starting with A (Rock)
+        if char1 == 'A' and char2 == 'X':
+            score += 1+3
+        elif char1 == 'A' and char2 == 'Y':
+            score += 2+6
+        elif char1 == 'A' and char2 == 'Z':
+            score += 3+0
+
+        #starting with B (Paper)
+        elif char1 == 'B' and char2 == 'X':
+            score += 1+0
+        elif char1 == 'B' and char2 == 'Y':
+            score += 2+3
+        elif char1 == 'B' and char2 == 'Z':
+            score += 3+6
+
+        #starting with C (Scissors)
+        elif char1 == 'C' and char2 == 'X':
+            score += 1+6
+        elif char1 == 'C' and char2 == 'Y':
+            score += 2+0
+        elif char1 == 'C' and char2 == 'Z':
+            score += 3+3
+
+        else:
+            print("fout")
+    print(score)
+
+
